1ea7ecc0e7d396080d52670b40e188a61c10e40b
CWMyster <infiniteaxis@gmail.com>
2016-09-07 18:54:22 -0500

    hello

--- js/script.js
+++ js/script.js
@@ -7,14 +7,17 @@ function sayHello() {
 
     // Make a variable called name and set it to your first name.
 
+var name = "Chris";
 
     //Make a variable called message and using the name variable from above,
     // set it to be something like:  "Hello, Mary.  Very nice to meet you."
+    
+var message = "Hello " + name + ", greetings!"
 
 
     // Log the message to the console.  Use console.log()
 
-
+console.log(message);
 
 }
 
@@ -22,53 +25,71 @@ function evenOrOdd() {
 
     // Make a variable called number, and set it to a number between 1 and 10
 
-
+var number = 7;
+    
     // Write a message (not just the number) to the console saying the value of the number
-
+    
+console.log("your number is " + number)
 
     // Use the modulus operator and an if statement to display a message
     // in the console that says whether the number is even or odd
-
-
-
-
+    
+if (number % 2 === 1) {
+  console.log("the number " + number + " is odd");
+}
+else {
+  console.log("the number " + number + " is even");
+}   
 }
 
 function countToTen() {
 
     // Write a for loop to count from 1 to 10
     // Use console.log() inside the loop to write out each number
-
-
-
-
+    
+for (num = 1; num <= 10; num++)
+    console.log("the number is now: " + num);
 }
 
 function addNumbers() {
 
     // Make an array of 5 numbers, at least one number should be negative
 
-
+var numsArray = [-100, 1, 2, 3, 5, 8];
+    
     // Write the array to the console
 
+console.log(numsArray);
 
     // Make a variable called total, set it to 0
 
+var total = 0;
 
-    // Use a for loop to set total to the sum of all of the numbers in the array
+     // Use a for loop to set total to the sum of all of the numbers in the array
 
+for (var i = 0; i < numsArray.length; i++) {
+  total = (numsArray[i] + numsArray[i]);
+}
+    
+    //I'm reallly stuck here, I can retrieve the last iteration but am embarrasingly not sure how to perform the math on each value. I've been using a different book that I do like but I'll be purchasing the recommended book at the end of the week.
+    
+   // Write a message to the console saying the value of total
 
-
-
-    // Write a message to the console saying the value of total
-
-
+console.log(total);
+    
     // Use an if statement to write a message to the console that says
     // whether total is greater than, less than, or equal to 50.
 
-
+if (total < 50) {
+  console.log("The total is less than 50");
 }
-
+else if (total > 50) {
+  console.log("The total is more than 50");
+}
+else if (total === 50) {
+  console.log("The total is 50");
+}
+}
 
 
 
