88f6852fb4ffb62b229d275984097b8a90471a9a
Hieu Pham <me4219fs@my.hennepintech.edu>
2016-09-07 15:56:07 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ function countToTen() {
 var i;
     // Write a for loop to count from 1 to 10
     // Use console.log() inside the loop to write out each number
-for (i=1; i<= 10;){
+for (i=1; i<= 10; i++){
     console.log(i);
 }
 
