a64fde3b422711add8af6abdd9038a9236b14d9c
Hieu Pham <me4219fs@my.hennepintech.edu>
2016-09-07 17:07:54 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,10 +24,11 @@ function evenOrOdd() {
 var number = {
     var i;
     0 < i <= 10;
+    i++;
 }
 
     // Write a message (not just the number) to the console saying the value of the number
-
+console.log("The number is: " + number);
 
     // Use the modulus operator and an if statement to display a message
     // in the console that says whether the number is even or odd
