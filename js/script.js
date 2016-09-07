748648d8a4c7316a313c57c29a71778b81be0863
Hieu Pham <me4219fs@my.hennepintech.edu>
2016-09-07 16:08:32 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ console.log(message);
 
 
 }
-
+var i
 function evenOrOdd() {
 
     // Make a variable called number, and set it to a number between 1 and 10
@@ -38,10 +38,9 @@ function countToTen() {
 
     // Write a for loop to count from 1 to 10
     // Use console.log() inside the loop to write out each number
-
-
-
-
+for (i=1; i<= 10; i++){
+    console.log(i);
+    }
 }
 
 function addNumbers() {
